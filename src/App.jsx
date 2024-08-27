e97b27bf35bf4adfe947f3c0d8f71233ef7918f9
memoryDev <115910144+memoryDev@users.noreply.github.com>
2024-08-27 15:51:36 +0900

    axios 추가 및  api 통신 테스트

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function App() {
-  return <></>;
+  const [result, setResult] = useState("faild");
+  useEffect(() => {
+    axios
+      .get("/api/test")
+      .then((response) => {
+        setResult(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return <>통신 성공 여부 = {result}</>;
 }
 
 export default App;
