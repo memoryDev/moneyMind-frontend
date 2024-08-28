6a141562fc54515881a40f389faebab6efaed1f3
memoryDev <115910144+memoryDev@users.noreply.github.com>
2024-08-28 16:34:57 +0900

    기본설정 수정

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,6 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
-  const [result, setResult] = useState("faild");
-  useEffect(() => {
-    axios
-      .get("/api/test")
-      .then((response) => {
-        setResult(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-
   return <>통신 성공 여부 = {result}</>;
 }
 
