eefa589927ea8ce235b5e70e41ac9068104f5fa2
memoryDev <115910144+memoryDev@users.noreply.github.com>
2024-09-03 20:52:53 +0900

    로그인 - 응답받은 token LocalStorage에 저장

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,50 @@
 import "./Login.css";
+import { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
+  const [state, setState] = useState({
+    userid: "",
+    password: "",
+  });
+
+  const stateHandler = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const loginHandler = () => {
+    try {
+      const formData = new FormData();
+      formData.append("username", state.userid);
+      formData.append("password", state.password);
+
+      axios
+        .post("http://localhost:9000/login", formData)
+        .then((res) => {
+          // 만약 api 통신 실패했을시
+          if (res.status !== 200) {
+            alert("API 통신에 실패 하였습니다.");
+            return;
+          }
+
+          const token = res.headers.authorization.replace("Bearer ", "");
+          console.log("token : " + token);
+
+          // localStorage에 JWT 토큰 저장
+          localStorage.setItem("token", token);
+        })
+        .catch((error) => {
+          console.log("===== error =====");
+          console.log(error);
+        });
+    } catch (error) {
+      console.error("login failed", error);
+    }
+  };
+
   return (
     <>
       <div className="login_wrap">
@@ -9,19 +53,27 @@ const Login = () => {
           <div className="login_input_box">
             <input
               type="text"
-              id="login_id"
+              name="userid"
+              id="userid"
               className="input"
               placeholder="아이디"
+              value={state.userid}
+              onChange={stateHandler}
             />
             <input
               type="password"
-              id="login_pw"
+              name="password"
+              id="password"
               className="input"
               placeholder="비밀번호"
+              value={state.password}
+              onChange={stateHandler}
             />
           </div>
           <div className="login_btn_wrap">
-            <button className="active">로그인</button>
+            <button className="active" onClick={loginHandler}>
+              로그인
+            </button>
             <button>회원가입</button>
           </div>
         </div>
